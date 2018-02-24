93038276a18af6d5cba1a1c8fea483aa5592a7b3
ciroalo <36788979+ciroalo@users.noreply.github.com>
2018-02-24 11:57:41 +0100

    Basic structure setup
    Basic PHP Structure setup, CSS and JS files linked to index

---
+++ iot_cloud/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>IOT Cloud</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <script src="scripts/script.js"></script>
+</head>
+<body>
+    <h2>Hola Mundo</h2>
+</body>
+</html>
